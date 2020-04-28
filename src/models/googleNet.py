7fc1f1a721babf72d97367496ff9be1af611dfbb
Cuijpie <jimcuijpers@gmail.com>
2020-04-28 04:04:08 +0200

    GoogleNet done

--- src/models/googleNet.py
+++ src/models/googleNet.py
@@ -1,10 +1,17 @@
-from keras.layers import Input, Convolution2D, MaxPooling2D, BatchNormalization
-from keras.layers import Concatenate
+from keras.layers import Input, \
+                            Convolution2D, \
+                            MaxPooling2D, \
+                            BatchNormalization, \
+                            AveragePooling2D, \
+                            Concatenate, \
+                            Flatten, \
+                            Dense, \
+                            Dropout
 from keras.models import Model
 
 
 class GoogleNet(object):
-    def __init__(self):
+    def __init__(self) -> None:
         self.inception_3a = [64, (96, 128), (16, 32), 32]
         self.inception_3b = [128, (128, 192), (32, 96), 64]
         self.inception_4a = [192, (96, 208), (16, 48), 64]
@@ -32,10 +39,29 @@ class GoogleNet(object):
         layer = self.__inception_layer(layer, self.inception_3a)
         layer = self.__inception_layer(layer, self.inception_3b)
 
-        self.model = Model()
+        layer = MaxPooling2D(pool_size=(3, 3), strides=2, padding='same')(layer)
+        layer = self.__inception_layer(layer, self.inception_4a)
 
-    @staticmethod
-    def __inception_layer(prev_layer, filters):
+        softmax_0 = self.__softmax_layer(layer, "softmax_0")
+
+        layer = self.__inception_layer(layer, self.inception_4b)
+        layer = self.__inception_layer(layer, self.inception_4c)
+        layer = self.__inception_layer(layer, self.inception_4d)
+
+        softmax_1 = self.__softmax_layer(layer, "softmax_1")
+        layer = self.__inception_layer(layer, self.inception_4e)
+
+        layer = MaxPooling2D(pool_size=(3, 3), strides=2, padding='same')(layer)
+        layer = self.__inception_layer(layer, self.inception_5a)
+        layer = self.__inception_layer(layer, self.inception_5b)
+        layer = AveragePooling2D(pool_size=(7, 7), strides=1, padding='valid')(layer)
+        layer = Flatten()(layer)
+        layer = Dropout(0.4)(layer)
+        softmax_2 = Dense(units=1000, activation='softmax', name="softmax_2")(layer)
+
+        self.model = Model(input_layer, [softmax_0, softmax_1, softmax_2])
+
+    def __inception_layer(self, prev_layer, filters) -> Concatenate:
         conv1x1 = Convolution2D(filters[0], kernel_size=1, strides=1, padding='same', activation='relu')(prev_layer)
 
         conv1x1_conv3x3 = Convolution2D(filters[1][0], kernel_size=1, strides=1, padding='same', activation='relu')(prev_layer)
@@ -49,8 +75,14 @@ class GoogleNet(object):
 
         return Concatenate(axis=-1)([conv1x1, conv1x1_conv3x3, conv1x1_conv5x5, max3x3_conv1x1])
 
-
-
+    def __softmax_layer(self, prev_layer, name):
+        layer = AveragePooling2D(pool_size=(5, 5), strides=3, padding='valid')(prev_layer)
+        layer = Convolution2D(filters=128, kernel_size=1, strides=1, padding='same', activation='relu')(layer)
+        layer = Flatten()(layer)
+        layer = Dense(units=1024, activation='relu')(layer)
+        layer = Dropout(0.7)(layer)
+        layer = Dense(units=1000, activation='softmax', name=name)(layer)
+        return layer
 
 
 
