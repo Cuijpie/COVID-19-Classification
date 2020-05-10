1560dd35ac56f72e3b4aab72765f3768c3c4e190
621Alice <15251608@life.hkbu.edu.hk>
2020-05-10 23:24:31 +0200

    preprocessing and training v1

---
+++ src/data_augmentation.py
@@ -0,0 +1,62 @@
+import matplotlib
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from keras.preprocessing.image import ImageDataGenerator
+from keras.utils import np_utils
+from imutils import paths
+import numpy as np
+import cv2
+
+
+matplotlib.use("Agg")
+
+dir = './data'
+Normal=dir+'/Normal'
+COVID=dir+'/COVID-19'
+BP=dir+'/BacterialPneumonia'
+VP=dir+'/ViralPneumonia'
+
+imagePaths1 = list(paths.list_images(Normal))
+imagePaths2 = list(paths.list_images(COVID))
+imagePaths3 = list(paths.list_images(BP))
+imagePaths4 = list(paths.list_images(VP))
+data = []
+labels = []
+print('number of images per folder: ',len(imagePaths1),len(imagePaths2),len(imagePaths3),len(imagePaths4))
+
+#loop through images in different folders and append each image and its label to two lists accordingly
+def loop_img(imagePaths, dirName):
+    for imagePath in imagePaths:
+        label = dirName
+        image = cv2.imread(imagePath)
+        image = cv2.resize(image, (224, 224))#image risize
+        data.append(image)
+        labels.append(label)
+
+loop_img(imagePaths1, 'Normal')
+loop_img(imagePaths2, 'COVID')
+# loop_img(imagePaths1, 'BP')
+# loop_img(imagePaths1, 'VP')
+
+print('number of images in data:',len(data))
+print('number of labels: ',len(labels))
+
+data = np.array(data, dtype="float") / 255.0
+labels = LabelEncoder().fit_transform(labels)
+labels = np_utils.to_categorical(labels, 2)
+print(data.shape[1:], labels.shape[0])
+# print(data[0])
+# print(labels[0])
+
+(trainX, testX, trainY, testY) = train_test_split(data, labels,
+	test_size=0.25, random_state=42)
+
+aug = ImageDataGenerator(rotation_range=20,
+                         zoom_range=0.15,
+                         width_shift_range=0.2,
+                         height_shift_range=0.2,
+                         shear_range=0.15,
+                         horizontal_flip=True,
+                         fill_mode="nearest")
+
+
---
+++ src/training_and_testing_v1.py
@@ -0,0 +1,47 @@
+from data_augmentation import *
+from models.googleNet import GoogleNet
+from sklearn.metrics import classification_report
+from keras.optimizers import SGD
+import matplotlib.pyplot as plt
+from tensorflow import keras
+import tensorflow as tf
+from keras.applications.mobilenet import MobileNet
+from keras.layers import GlobalAveragePooling2D, Dense, Dropout, Flatten
+from keras.models import Sequential
+
+# input=tf.keras.Input(shape=data.shape, dtype="float")#input layer with the shape of the training data, currently it's (940,224,224,3)(940 images of 64x64 pixels in RGB (3 channels)))
+EPOCHS=50
+BatchSize=8
+# opt=keras.optimizers.Adam(learning_rate=0.01)
+opt = SGD(lr=1e-2, momentum=0.9, decay=1e-2 / EPOCHS)
+
+base_mobilenet_model = MobileNet(input_shape =  data.shape[1:],
+                                 include_top = False, weights = None)
+model = Sequential()
+model.add(base_mobilenet_model)
+model.add(GlobalAveragePooling2D())
+model.add(Dropout(0.5))
+model.add(Dense(512))
+model.add(Dropout(0.5))
+model.add(Dense(labels.shape[1], activation = 'sigmoid'))
+model.summary()
+
+model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
+H = model.fit_generator(aug.flow(trainX, trainY, batch_size=BatchSize), validation_data=(testX, testY), steps_per_epoch=len(trainX) // BatchSize, epochs=EPOCHS)
+
+predictions = model.predict(testX, batch_size=BatchSize)
+print(classification_report(testY.argmax(axis=1), predictions.argmax(axis=1), target_names=LabelEncoder().classes_))
+
+# plot the training loss and accuracy
+N = np.arange(0, EPOCHS)
+plt.style.use("ggplot")
+plt.figure()
+plt.plot(N, H.history["loss"], label="train_loss")
+plt.plot(N, H.history["val_loss"], label="val_loss")
+plt.plot(N, H.history["acc"], label="train_acc")
+plt.plot(N, H.history["val_acc"], label="val_acc")
+plt.title("Training Loss and Accuracy on Dataset")
+plt.xlabel("Epoch #")
+plt.ylabel("Loss/Accuracy")
+plt.legend(loc="lower left")
+plt.show()
