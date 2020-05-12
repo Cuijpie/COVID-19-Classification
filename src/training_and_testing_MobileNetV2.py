26dbce488915c04ef0fa18fa4ea0a3a70fbfe6eb
621Alice <15251608@life.hkbu.edu.hk>
2020-05-12 13:27:29 +0200

    Update training_and_testing_MobileNetV2.py

--- src/training_and_testing_MobileNetV2.py
+++ src/training_and_testing_MobileNetV2.py
@@ -14,8 +14,8 @@ from keras.models import Sequential
 # input=tf.keras.Input(shape=data.shape, dtype="float")#input layer with the shape of the training data, currently it's (940,224,224,3)(940 images of 64x64 pixels in RGB (3 channels)))
 EPOCHS=30
 BatchSize=8
-# opt=keras.optimizers.Adam(learning_rate=0.01)
-opt = SGD(lr=1e-2, momentum=0.9, decay=1e-2 / EPOCHS)
+opt=keras.optimizers.Adam(learning_rate=0.01)
+# opt = SGD(lr=1e-2, momentum=0.9, decay=1e-2 / EPOCHS)
 
 #MobileNet
 base_model = MobileNetV2(input_shape =  data.shape[1:],
