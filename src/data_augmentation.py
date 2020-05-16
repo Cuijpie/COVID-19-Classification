7b59b2a04121a3cb667866e08d2d5604683a69db
Cuijpie <jimcuijpers@gmail.com>
2020-05-16 21:53:20 +0200

    more

--- src/data_augmentation.py
+++ src/data_augmentation.py
@@ -1,51 +1,11 @@
 import matplotlib
-from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split
 from keras.preprocessing.image import ImageDataGenerator, load_img, array_to_img, img_to_array
-from keras.utils import np_utils
-from imutils import paths
-import numpy as np
-import cv2
 
 matplotlib.use("Agg")
 
 dir = './data'
 Normal = dir + '/Normal'
 Covid = dir + '/COVID-19'
-BP = dir + '/BacterialPneumonia'
-VP = dir + '/ViralPneumonia'
-
-imagePaths1 = list(paths.list_images(Normal))
-imagePaths2 = list(paths.list_images(Covid))
-imagePaths3 = list(paths.list_images(BP))
-imagePaths4 = list(paths.list_images(VP))
-data = []
-labels = []
-print('number of images per folder: ', len(imagePaths1), len(imagePaths2), len(imagePaths3), len(imagePaths4))
-
-
-# loop through images in different folders and append each image and its label to two lists accordingly
-def loop_img(imagePaths, dirName):
-    for imagePath in imagePaths:
-        label = dirName
-        image = cv2.imread(imagePath)
-        image = cv2.resize(image, (224, 224))  # image risize
-        data.append(image)
-        labels.append(label)
-
-loop_img(imagePaths1, 'Normal')
-loop_img(imagePaths2, 'COVID')
-loop_img(imagePaths1, 'BP')
-loop_img(imagePaths1, 'VP')
-
-print('number of images in data:', len(data))
-print('number of labels: ', len(set(labels)))
-
-data = np.array(data, dtype="float") / 255.0
-le = LabelEncoder()
-labels = le.fit_transform(labels)
-labels = np_utils.to_categorical(labels, 4)
-print(data.shape[1:], labels.shape[0])
 
 data_gen = ImageDataGenerator(
         width_shift_range=0.2,
@@ -55,23 +15,17 @@ data_gen = ImageDataGenerator(
         horizontal_flip=True,
         fill_mode='nearest')
 
-img = load_img(Covid + '/3.jpeg')  # this is a PIL image
-x = img_to_array(img)
-x = x.reshape((1,) + x.shape)
-
-i = 0
-
-for batch in data_gen.flow(x, batch_size=1, save_to_dir=Covid, save_prefix='covid', save_format='jpeg'):
-    i += 1
-
-    if i > 300:
-        break
-
-
-# (trainX, testX, trainY, testY) = train_test_split(data, labels, test_size=0.3, random_state=42)
+#for imgPath in imagePaths2:
+#    img = load_img(imgPath)
+#    x = img_to_array(img)
+#    x = x.reshape((1,) + x.shape)
+#    i = 0
+#    for batch in data_gen.flow(x, batch_size=1, save_to_dir=Covid, save_prefix='aug', save_format='jpeg'):
+#        i += 1
+#        if i > 10:
+#            break
 
 train_datagen = ImageDataGenerator(
-        rescale=1./255,
         shear_range=0.2,
         zoom_range=0.2,
         horizontal_flip=True)
@@ -85,20 +39,20 @@ test_datagen = ImageDataGenerator(rescale=1./255)
 # batches of augmented image data
 train_generator = train_datagen.flow_from_directory(
         './data',  # this is the target directory
-        target_size=(224, 224),  # all images will be resized to 150x150
+        target_size=(224, 224),
         batch_size=32,
-        class_mode='binary')  # since we use binary_crossentropy loss, we need binary labels
+        class_mode='categorical')
 
 # this is a similar generator, for validation data
 validation_generator = test_datagen.flow_from_directory(
         './val',
         target_size=(224, 224),
         batch_size=32,
-        class_mode='binary')
+        class_mode='categorical')
 
 generator = data_gen.flow_from_directory(
         './val',
         target_size=(224, 224),
         batch_size=32,
-        class_mode='binary',  # only data, no labels
-        shuffle=False)  # keep data in same order as labels
+        class_mode='categorical',
+        shuffle=False)
--- src/inceptionV3.py
+++ src/inceptionV3.py
@@ -1,31 +1,32 @@
 
-from sklearn.metrics import classification_report, confusion_matrix, plot_confusion_matrix
+from sklearn.metrics import classification_report, confusion_matrix
 from data_augmentation import *
-import matplotlib.pyplot as plt
+from keras.optimizers import Adam
 from keras.applications import InceptionV3
 from keras.layers import GlobalAveragePooling2D, Dense, Dropout, BatchNormalization, Flatten
 from keras.models import Model
+import numpy as np
 
-BATCH_SIZE = 32
+BATCH_SIZE = 64
 EPOCHS = 50
-CLASSES = 4
 
 base_model = InceptionV3(weights='imagenet', include_top=False)
 x = base_model.output
-#x = Flatten()(x)
 x = Dropout(0.5)(x)
 x = GlobalAveragePooling2D()(x)
 x = Dense(128, activation='relu')(x)
 x = BatchNormalization()(x)
-predictions = Dense(1, activation='sigmoid')(x)
+predictions = Dense(3, activation='softmax')(x)
 model = Model(inputs=base_model.input, outputs=predictions)
 
 # transfer learning
 for layer in base_model.layers:
     layer.trainable = False
 
-model.compile(loss="binary_crossentropy",
-              optimizer='adam',
+opt = Adam(lr=0.001)
+
+model.compile(loss="categorical_crossentropy",
+              optimizer=opt,
               metrics=["accuracy"])
 
 # train the network
@@ -33,13 +34,15 @@ print("[INFO] training network...")
 H = model.fit_generator(
     train_generator,
     steps_per_epoch=2000 // BATCH_SIZE,
-    epochs=EPOCHS,
+    epochs=EPOCHS,#
     validation_data=validation_generator,
     validation_steps=800 // BATCH_SIZE)
 
 model.save_weights('test_inception.h5')
 
-probabilities = model.predict_generator(generator, 601 // BATCH_SIZE + 1)
+#model.load_weights('test_inception.h5')
+
+probabilities = model.predict_generator(generator, 1607)
 
 y_true = validation_generator.classes
 y_pred = np.argmax(probabilities, axis=1)
@@ -48,7 +51,7 @@ print('Confusion Matrix')
 cm = confusion_matrix(y_true, y_pred)
 print(cm)
 print('Classification Report')
-print(classification_report(y_true, y_pred))
+print(classification_report(y_true, y_pred, target_names=['COVID', 'NORMAL', 'PNEUMONIA']))
 
 #plt.figure()
 #plt.ylabel("Loss (training and validation)")
