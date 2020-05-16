dc6b96a97785a09c27aae20c8e617a2217808bc6
Cuijpie <jimcuijpers@gmail.com>
2020-05-16 03:46:06 +0200

    test

--- src/data_augmentation.py
+++ src/data_augmentation.py
@@ -35,8 +35,8 @@ def loop_img(imagePaths, dirName):
 
 loop_img(imagePaths1, 'Normal')
 loop_img(imagePaths2, 'COVID')
-# loop_img(imagePaths1, 'BP')
-# loop_img(imagePaths1, 'VP')
+loop_img(imagePaths1, 'BP')
+loop_img(imagePaths1, 'VP')
 
 print('number of images in data:', len(data))
 print('number of labels: ', len(set(labels)))
@@ -44,7 +44,7 @@ print('number of labels: ', len(set(labels)))
 data = np.array(data, dtype="float") / 255.0
 le = LabelEncoder()
 labels = le.fit_transform(labels)
-labels = np_utils.to_categorical(labels, 2)
+labels = np_utils.to_categorical(labels, 4)
 print(data.shape[1:], labels.shape[0])
 
 data_gen = ImageDataGenerator(
@@ -55,7 +55,7 @@ data_gen = ImageDataGenerator(
         horizontal_flip=True,
         fill_mode='nearest')
 
-img = load_img(Covid + '/0.jpeg')  # this is a PIL image
+img = load_img(Covid + '/3.jpeg')  # this is a PIL image
 x = img_to_array(img)
 x = x.reshape((1,) + x.shape)
 
@@ -63,14 +63,42 @@ i = 0
 
 for batch in data_gen.flow(x, batch_size=1, save_to_dir=Covid, save_prefix='covid', save_format='jpeg'):
     i += 1
-    if i > 200:
+
+    if i > 300:
         break
 
 
-(trainX, testX, trainY, testY) = train_test_split(data, labels, test_size=0.3, random_state=42)
+# (trainX, testX, trainY, testY) = train_test_split(data, labels, test_size=0.3, random_state=42)
 
-aug = ImageDataGenerator(
+train_datagen = ImageDataGenerator(
         rescale=1./255,
         shear_range=0.2,
         zoom_range=0.2,
         horizontal_flip=True)
+
+# this is the augmentation configuration we will use for testing:
+# only rescaling
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+# this is a generator that will read pictures found in
+# subfolers of 'data/train', and indefinitely generate
+# batches of augmented image data
+train_generator = train_datagen.flow_from_directory(
+        './data',  # this is the target directory
+        target_size=(224, 224),  # all images will be resized to 150x150
+        batch_size=32,
+        class_mode='binary')  # since we use binary_crossentropy loss, we need binary labels
+
+# this is a similar generator, for validation data
+validation_generator = test_datagen.flow_from_directory(
+        './val',
+        target_size=(224, 224),
+        batch_size=32,
+        class_mode='binary')
+
+generator = data_gen.flow_from_directory(
+        './val',
+        target_size=(224, 224),
+        batch_size=32,
+        class_mode='binary',  # only data, no labels
+        shuffle=False)  # keep data in same order as labels
--- src/inceptionV3.py
+++ src/inceptionV3.py
@@ -1,41 +1,54 @@
-from sklearn.metrics import classification_report
+
+from sklearn.metrics import classification_report, confusion_matrix, plot_confusion_matrix
 from data_augmentation import *
 import matplotlib.pyplot as plt
 from keras.applications import InceptionV3
-from keras.layers import GlobalAveragePooling2D, Dense, Dropout, BatchNormalization
+from keras.layers import GlobalAveragePooling2D, Dense, Dropout, BatchNormalization, Flatten
 from keras.models import Model
 
-BATCH_SIZE = 64
-EPOCHS = 10
-CLASSES = 2
+BATCH_SIZE = 32
+EPOCHS = 50
+CLASSES = 4
 
 base_model = InceptionV3(weights='imagenet', include_top=False)
 x = base_model.output
+#x = Flatten()(x)
 x = Dropout(0.5)(x)
 x = GlobalAveragePooling2D()(x)
 x = Dense(128, activation='relu')(x)
 x = BatchNormalization()(x)
-predictions = Dense(2, activation='sigmoid')(x)
+predictions = Dense(1, activation='sigmoid')(x)
 model = Model(inputs=base_model.input, outputs=predictions)
 
 # transfer learning
 for layer in base_model.layers:
     layer.trainable = False
 
-model.compile(loss="categorical_crossentropy",
+model.compile(loss="binary_crossentropy",
               optimizer='adam',
               metrics=["accuracy"])
 
 # train the network
 print("[INFO] training network...")
 H = model.fit_generator(
-    aug.flow(trainX, trainY, batch_size=BATCH_SIZE),
-    validation_data=(testX, testY),
-    steps_per_epoch=len(trainX) // BATCH_SIZE,
-    epochs=EPOCHS, verbose=1)
+    train_generator,
+    steps_per_epoch=2000 // BATCH_SIZE,
+    epochs=EPOCHS,
+    validation_data=validation_generator,
+    validation_steps=800 // BATCH_SIZE)
 
-predictions = model.predict(testX, batch_size=BATCH_SIZE)
-print(classification_report(testY.argmax(axis=1), predictions.argmax(axis=1), target_names=le.classes_))
+model.save_weights('test_inception.h5')
+
+probabilities = model.predict_generator(generator, 601 // BATCH_SIZE + 1)
+
+y_true = validation_generator.classes
+y_pred = np.argmax(probabilities, axis=1)
+
+print('Confusion Matrix')
+cm = confusion_matrix(y_true, y_pred)
+print(cm)
+print('Classification Report')
+print(classification_report(y_true, y_pred))
 
 #plt.figure()
 #plt.ylabel("Loss (training and validation)")
